d3634b5a2081303b683f10425a27317eda54602f
shao <s1976189@gmail.com>
2011-07-21 11:05:33 +0800

    7.21 first7.21 first7.21 first7.21 first7.21 first7.21 first7.21 first7.21 first7.21 first7.21 first7.21 first7.21 first7.21 first7.21 first7.21 first7.21 first7.21 first7.21 first7.21 first7.21 first7.21 first

--- shao.c
+++ shao.c
@@ -1,6 +1,12 @@
 # include <stdio.h>
+
 int main(int argc, const char *argv[])
-{
-        printf("hello,this world!i'm comming!\n");
+{      
+	int s;
+	for (s = 0; s < 10000; s++)
+        {
+		printf("hello,this world!i'm comming!\n");
+	}
+	
 	return 0;
 }
